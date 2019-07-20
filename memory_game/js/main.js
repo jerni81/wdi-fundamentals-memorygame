24aece3ab2cfd2f266a62dfe6e5708b005c80886
AJ <aj@alexandras-mbp.home>
2019-07-20 13:40:29 -0400

    created game logic2

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,25 @@
+///
+
+
 var cards = ["Queen", "Queen", "King", "King"];
 var cardsInPlay = [];
-var cardOne = cards[2];
-var cardTwo = cards[2];
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardTwo');
-console.log("user flipped " + cardOne)
-console.log("user flipped " + cardTwo)
-if (cardsInPlay.length = 1) {if (cardsInPlay[0] === cardsInPlay[1])
-  { alert ("you found a match");
-} else { alert ("sorry, try again");
-}}
+
+
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("you found a match");}
+    else {console.log("sorry, try again");
+  }
+}
+
+function flipCard (cardId) {
+  console.log("user flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+}
+
+flipCard(0);
+flipCard(2);
